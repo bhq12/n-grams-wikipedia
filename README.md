2c7cb0e89d2cf70a662a3f6f52c4a046d6626f44
bhq12 <brook.queree@gmail.com>
2025-07-30 13:26:01 +1000

    Add bzip extraction to setup script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # n-grams-wikipedia
 An n-gram language model based on a wikipedia dump corpus
+
+NOTE: The setup script will download and extract ~150GB of text files, be prepared with enough disk space.
--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
-wget -c https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2
+wget -c https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2;
+pbzip2 -dkv1 ./enwiki-latest-pages-articles.xml.bz2

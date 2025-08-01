0b64401dd1c9e0e029fed3fb2809539e036f6033
bhq12 <brook.queree@gmail.com>
2025-08-01 16:33:01 +1000

    Count singletons

--- src/n-grams-wikipedia/naive_count.py
+++ src/n-grams-wikipedia/naive_count.py
@@ -8,7 +8,7 @@ def print_significant_words(word_counts: dict):
             print(f'word: {word} count: {count}')
 
 word_counts = defaultdict(int)
-
+singletons = set()
 total_words = 0
 
 with open("./enwiki-latest-pages-articles.xml") as corpus:
@@ -16,7 +16,11 @@ with open("./enwiki-latest-pages-articles.xml") as corpus:
         # naive word splitting
         for word in line.split(' '):
             word_counts[word] += 1
+            if word_counts[word] == 1:
+                singletons.add(word)
+            elif word in singletons:
+                singletons.remove(word)
             if total_words % 10000000 == 0:
-                print(total_words)
+                print(f'total_words: {total_words}, singletons: {len(singletons)}, singleton_percentage: {100 * len(singletons) / (total_words if total_words != 0 else 1)}')
                 print(print_significant_words(word_counts))
             total_words += 1
